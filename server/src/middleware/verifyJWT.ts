95b8fc5813a29524ff1e2b0390dcc1bf2114ff9a
Sergii <apserbud22@gmail.com>
2023-01-31 15:29:41 +0200

    add refresh token to verifyJWT

--- server/src/middleware/verifyJWT.ts
+++ server/src/middleware/verifyJWT.ts
@@ -1,10 +1,25 @@
 import { Response, Request, NextFunction } from "express";
+import JWT from "jsonwebtoken";
+import { jwtSecret } from "../config/keys";
 
 export const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
   try {
-    const token = req.cookies;
+    const { token } = req.cookies;
 
-    if ("token" in token) {
+    if (typeof token === "string") {
+      const decoded = JWT.decode(token);
+      if (decoded === null || typeof decoded === "string") {
+        return res.status(401).json({ msg: "You need auth" });
+      }
+      const { email, id } = decoded;
+      const freshToken = JWT.sign({ email, id }, jwtSecret, {
+        expiresIn: 60 * 60,
+      });
+
+      res.cookie("token", freshToken, {
+        httpOnly: true,
+        maxAge: 60 * 60 * 1000,
+      });
       return next();
     }
     res.status(401).json({ msg: "You need auth" });
